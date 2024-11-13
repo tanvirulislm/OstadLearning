5731ee649bf0d4b76ad47e84b13d9a1a207f6c02
Tanvirul <ownbinodon@gmail.com>
2024-11-13 15:30:02 +0600

    OK

--- Module 10/practice.sql
+++ Module 10/practice.sql
@@ -1,65 +1,18 @@
-SELECT * FROM artists; 
-SELECT Name, Album_id FROM `Tracks` ;
-SELECT * FROM `Artists` LIMIT 5;
+-- Active: 1731477436449@@127.0.0.1@3306@newproject
+SELECT `firstName`, `lastName`, `email` FROM users
 
-SELECT * FROM `Artists` LIMIT 10,5; //get 5 records starting from 10th record
-SELECT * FROM `Albums` LIMIT 10,5;
+SELECT firstName,lastName,email FROM users
 
-SELECT * FROM `artists` ORDER BY name;
+SELECT * FROM users WHERE id=10
 
-SELECT * FROM `artists` ORDER BY name DESC;
+SELECT * FROM users WHERE firstName='Sam'
 
-SELECT * FROM `Artists` WHERE id=16;
+DELETE FROM users WHERE firstName='Sam'
 
-SELECT * FROM `Albums` WHERE id <= 16;
+INSERT INTO users (firstName,lastName,email,mobile,password,otp) VALUES ('Tanvirul', 'Islam', 'tanvir@tanvir.com', '01454455656', 'encrypted_password1', '123456')
 
-SELECT * FROM `Albums` WHERE id < 16;
-
-SELECT * FROM `Albums` WHERE Title LIKE 'A%';
-
-SELECT * FROM `Albums` WHERE Title LIKE '%o';
-
-SELECT * FROM `Albums` WHERE Title = "Cafezinho";
-
-SELECT * FROM `Albums` WHERE Title LIKE 'A%' AND Title like '%o';
-
-SELECT * FROM `Albums` WHERE Title LIKE 'A%o'; //same
-
-SELECT * FROM `Albums` WHERE Title LIKE 'A%' OR Title like '%o';
+UPDATE users SET `firstName` = 'Khairul', `lastName` = 'Islam' WHERE id = 11
 
 
-SELECT count(*) FROM albums;
+UPDATE users SET `firstName` = 'Ariful', `lastName` = 'Islam' WHERE `firstName`='Tanvirul' AND `lastName`= 'Islam'
 
-SELECT count(*) FROM artists;
-
-SELECT count(*) FROM artists WHERE name like 'A%'
-
-SELECT count(*) FROM `Albums` WHERE Title LIKE 'A%' AND Title like '%o';
-
-
-SELECT sum(amount) FROM `loans` WHERE type='L';
-
-UPDATE loans SET type='L' WHERE id =5;
-UPDATE loans SET type='L' WHERE Name='Dulal';
-
-DELETE FROM `loans` WHERE id = 6;
-
-SELECT sum(amount), type FROM `loans` GROUP BY type;
-
-SELECT count(*), Genre_id FROM `Tracks` group by Genre_id;
-
-SELECT count(*), Genre_id FROM `Tracks` group by Genre_id ORDER BY count(*);
-
-SELECT count(*), Genre_id FROM `Tracks` group by Genre_id ORDER BY count(*) DESC;
-
-SELECT count(*) as Total, Genre_id FROM `Tracks` group by Genre_id ORDER BY count(*) DESC;
-
-INSERT INTO loans VALUES(null, 'Miraz', 500, 'B', '2024-11-03', null);
-
-INSERT INTO loans (name, amount, type, `date`) VALUES('Munir', 500, 'L', '2024-11-03');
-
-INSERT INTO loans (name, amount, type, `date`) 
-VALUES 
-  ('Munir', 500, 'L', '2024-11-03'),
-  ('Ali', 750, 'B', '2024-11-02'),
-  ('Sara', 1000, 'L', '2024-11-01');
