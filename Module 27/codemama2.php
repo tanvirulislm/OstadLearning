84ead8e949f8559da03fe82fda744697dab9c1c8
Tanvirul <ownbinodon@gmail.com>
2025-03-13 12:52:38 +0600

    Codemama solved

--- Module 27/codemama2.php
+++ Module 27/codemama2.php
@@ -1,1 +1,32 @@
 <?php
+function removeKDigits($num, $k) {
+    $stack = [];
+    $length = strlen($num);
+    
+    if ($k >= $length) return "0";
+    
+    for ($i = 0; $i < $length; $i++) {
+        while ($k > 0 && !empty($stack) && end($stack) > $num[$i]) {
+            array_pop($stack);
+            $k--;
+        }
+        $stack[] = $num[$i];
+    }
+    
+
+    while ($k > 0) {
+        array_pop($stack);
+        $k--;
+    }
+    
+
+    $result = ltrim(implode("", $stack), "0");
+    return $result === "" ? "0" : $result;
+}
+
+
+fscanf(STDIN, "%d", $T);
+while ($T--) {
+    fscanf(STDIN, "%s %d", $N, $K);
+    echo removeKDigits($N, $K) . "\n";
+}
